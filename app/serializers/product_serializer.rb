2f3b55622b9f55d5ead4562b48b820836f4562ec
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-28 19:36:49 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -2,9 +2,9 @@ class ProductSerializer
   def self.serializer
     s_p = '{'
     s_p += '"id": "' + post.id.to_s + '", '
-    s_p += '"title": "' + post.name + '",'
+    s_p += '"name": "' + post.name + '",'
     s_p += '"price": "' + post.price + '",'
-    s_p += '"title": "' + post.name + '",'
-    s_p += '"title": "' + post.name + '",'
+    s_p += '"inventory": "' + post.inventory + '",'
+    s_p += '"description": "' + post.description + '",'
   end
 end
